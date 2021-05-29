5f4d038db81e420ca0ee6aba12f190c2df3613b0
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-29 17:26:20 +0530

    Added:Regitration.java, Validate Lastname

--- Registration.java
+++ Registration.java
@@ -1,10 +1,32 @@
 package com.registration;
 
-public class RegistrationWelcome {
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+@FunctionalInterface
+interface FirstName {
+	
+	public abstract void validate_FirstName();
+}
+public class RegistrationUC1 {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Wlecome to Lemda Expression");
+		
+		Scanner scn = new Scanner(System.in);
+		FirstName myLamda = () -> {
+			String Regex = "^[A-Z]{1}[a-zA-Z]{2,25}$";
+			System.out.println("Enter a First name:");
+			String FirstName = scn.next();
+			Pattern pt =Pattern.compile(Regex);
+			if (pt.matches(Regex, FirstName)) {
+				System.out.println("valid FirstName");
+			} else {
+				System.out.println("Invalid FirstName");
+			}
+		};
+		myLamda.validate_FirstName();
 	}
-
 }
