28b4847b948f1a55f2eeec22f8a594eea0d84e91
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-29 17:20:32 +0530

    Added:RegestrationWelcome.java

---
+++ Registration.java
@@ -0,0 +1,10 @@
+package com.registration;
+
+public class RegistrationWelcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Wlecome to Lemda Expression");
+	}
+
+}
