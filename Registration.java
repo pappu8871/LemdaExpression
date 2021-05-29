e6b7991108eaa54a39117dd92aee58920f9e3156
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-29 17:32:21 +0530

    Added:UC-2, LastName Validation

--- Registration.java
+++ Registration.java
@@ -1,32 +1,29 @@
 package com.registration;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.util.function.Predicate;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 @FunctionalInterface
-interface FirstName {
-	
-	public abstract void validate_FirstName();
-}
-public class RegistrationUC1 {
-	
-	public static void main(String[] args) {
-		
-		Scanner scn = new Scanner(System.in);
-		FirstName myLamda = () -> {
-			String Regex = "^[A-Z]{1}[a-zA-Z]{2,25}$";
-			System.out.println("Enter a First name:");
-			String FirstName = scn.next();
-			Pattern pt =Pattern.compile(Regex);
-			if (pt.matches(Regex, FirstName)) {
-				System.out.println("valid FirstName");
-			} else {
-				System.out.println("Invalid FirstName");
-			}
-		};
-		myLamda.validate_FirstName();
+	interface LastName {
+	public abstract void validate_LastName();
 	}
-}
+
+	public class RegistrationUC2 {
+	public static void main(String[] args){
+	Scanner scn = new Scanner(System.in);
+	LastName myLamda = () -> {
+	String Regex = "^[A-Z]{1}[a-zA-Z]{2,25}$";
+	System.out.println("Enter a Last name:");
+	String LastName = scn.next();
+	Pattern pt =Pattern.compile(Regex);
+	if (Pattern.matches(Regex, LastName)) {
+	System.out.println("valid LastName");
+	} else {
+	System.out.println("Invalid LastName");
+	}
+	};
+	myLamda.validate_LastName();
+	}
+	}
+
+
