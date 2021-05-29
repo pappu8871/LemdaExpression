a0e3f4f854f27c7c3e60676edc782617d1f767e7
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-29 17:41:42 +0530

    Added:UC-4, MobileNumber

--- Registration.java
+++ Registration.java
@@ -1,30 +1,32 @@
 package com.registration;
 
 import java.util.Scanner;
+
 import java.util.regex.Pattern;
 
 @FunctionalInterface
-interface Emailid {
-public abstract void validate_email();
+interface MobileNum {
+public abstract void valid_Mobile();	
 }
-public class RegistrationUC3 {
 
-	public static void main(String[] args) {
+public class RegistrationUC4 {
+
+public static void main(String[] args) {
 		
 		Scanner scn = new Scanner(System.in);
-		Emailid myLamda = () -> {
+		MobileNum myLamda = () -> {
 		
-		String Regex = "^[a-z]{3,15}[.][a-z]{3,15}+[@][a-z]{5,10}[.][a-z]{2}[.][a-z]{2}$";
-		System.out.println("Enter a email id:");
-		String Emailid = scn.next();
+		String Regex = "^[91]{2}\s{0,1}[789]{1}[0-9]{9}$";
+		System.out.println("Enter a Mobile Number:");
+		String Mob = scn.next();
 		Pattern pt =Pattern.compile(Regex);
-		if (Pattern.matches(Regex, Emailid)) {
-		System.out.println("valid emailid");
+		if (Pattern.matches(Regex, Mob)) {
+		System.out.println(" Mobile Number is Valid");
 		} else {
-		System.out.println("Invalid emailid");
+		System.out.println("Mobile Number is  InValid");
 		}
 		};
-		myLamda.validate_email();
+		myLamda.valid_Mobile();
 		}
 		}
 
