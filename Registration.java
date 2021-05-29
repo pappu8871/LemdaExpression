e938a7ae73a4b3ea7efc8d6810b4b2146e9e1e0c
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-29 18:09:58 +0530

    Added:UC-8, added Special Charactor

--- Registration.java
+++ Registration.java
@@ -3,27 +3,27 @@ package com.registration;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 @FunctionalInterface
-interface Password2 {  
+interface Password3 {
 
-	void validate_Pass();
-	
+public abstract	void validate_pass();
+
 }
-public class RegistrationUC7 {
+public class RegistrationUC8 {
 public static void main(String[] args) {
 		
 		Scanner scn = new Scanner(System.in);
-		Password2 myLamda = () -> {
-		
-		String Regex = "^[a-zA-Z0-9]{8,25}$";
-		System.out.println("Enter your Password:");
-		String password = scn.next();
-		Pattern pt =Pattern.compile(Regex);
-		if (Pattern.matches(Regex, password)) {
-		System.out.println("valid password");
-		} else {
-		System.out.println("Invalid password");
-		}
+		Password3 myLamda = () -> {
+			
+			String Regex = "^[a-zA-Z0-9@#$%^&+=_-]{8,25}$";
+			System.out.println("Enter Your Password:");
+			String password = scn.next();
+			Pattern pt =Pattern.compile(Regex);
+			if (pt.matches(Regex, password)) {
+				System.out.println("valid Password");
+			} else {
+				System.out.println("Invalid Password");
+			}
 		};
-		myLamda.validate_Pass();
-		}
+		myLamda.validate_pass();
+	}
 }
